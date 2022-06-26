4de1992fa9e77571071f6de9690e81a8236a1396
tgitlawrie <tlawrie@orcon.net.nz>
2022-06-26 16:07:54 +1200

    player positions and cards

--- src/features/showTeam/MyTeam.js
+++ src/features/showTeam/MyTeam.js
@@ -17,37 +17,58 @@ const MyTeam = () => {
   const gCard = PlayerCard("G");
 
   return (
-    <div className="board">
-      <div className="col-lg-10 h-100 offset-1">
-        <div className="row h-25 m-0">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
-        <div className="row h-25 m-0">
-          <div className="col d-flex align-items-center justify-content-end">
-            {lwCard}
-          </div>
-          <div className="col d-flex align-items-center justify-content-center">
-            {cCard}
-          </div>
-          <div className="col d-flex align-items-center">{rwCard}</div>
-        </div>
-        <div className="row h-25 m-0">
-          <div className="col d-flex align-items-center justify-content-end">
-            {ldCard}
-          </div>
-          <div className="col"></div>
-          <div className="col d-flex align-items-center">{rdCard}</div>
-        </div>
-        <div className="row h-25 m-0">
-          <div className="col"></div>
-          <div className="col align-items">{gCard}</div>
-          <div className="col"></div>
-        </div>
+    <div className="board line">
+      <div className="" id="left-w">
+        {lwCard}
+      </div>
+      <div className="" id="center">
+        {cCard}
+      </div>
+      <div className="" id="right-w">
+        {rwCard}
+      </div>
+      <div className="" id="left-d">
+        {ldCard}
+      </div>
+      <div className="" id="right-d">
+        {rdCard}
+      </div>
+      <div className="" id="g">
+        {gCard}
       </div>
     </div>
   );
 };
 
 export default MyTeam;
+
+/* <div className="board">
+  <div className="col-lg-10 h-100 offset-1">
+    <div className="row h-25 m-0">
+      <div className="col"></div>
+      <div className="col"></div>
+      <div className="col"></div>
+    </div>
+    <div className="row h-25 m-0">
+      <div className="col d-flex align-items-center justify-content-end">
+        {lwCard}
+      </div>
+      <div className="col d-flex align-items-center justify-content-center">
+        {cCard}
+      </div>
+      <div className="col d-flex align-items-center">{rwCard}</div>
+    </div>
+    <div className="row h-25 m-0">
+      <div className="col d-flex align-items-center justify-content-end">
+        {ldCard}
+      </div>
+      <div className="col"></div>
+      <div className="col d-flex align-items-center">{rdCard}</div>
+    </div>
+    <div className="row h-25 m-0">
+      <div className="col"></div>
+      <div className="col align-items">{gCard}</div>
+      <div className="col"></div>
+    </div>
+  </div>
+</div>; */
